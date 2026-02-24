e124dad4e1125c96360972da31664f9403dc0a83
Julian Easterling <julian@julianscorner.com>
2026-02-23 21:59:05 -0500

    List all LXC containers/KVM VMs

---
+++ cmd/lxc/list.go
@@ -0,0 +1,77 @@
+/*
+Copyright © 2026 Julian Easterling
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package lxc
+
+import (
+	"fmt"
+
+	"github.com/dcjulian29/proxmoxctl/internal/api"
+	"github.com/dcjulian29/proxmoxctl/internal/output"
+	"github.com/spf13/cobra"
+)
+
+func listCmd() *cobra.Command {
+	var node string
+	cmd := &cobra.Command{
+		Use:   "list",
+		Short: "List all LXC containers",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			client, err := api.New()
+			if err != nil {
+				return err
+			}
+
+			if node == "" {
+				node, err = client.DefaultNode()
+				if err != nil {
+					return err
+				}
+			}
+
+			var resp struct {
+				Data []map[string]any `json:"data"`
+			}
+
+			if err := client.Get(fmt.Sprintf("/nodes/%s/lxc", node), &resp); err != nil {
+				return err
+			}
+
+			if output.IsJSON() {
+				return output.JSON(resp.Data)
+			}
+
+			headers := []string{"VMID", "NAME", "STATUS", "MEM(MB)"}
+			rows := make([][]string, 0, len(resp.Data))
+
+			for _, v := range resp.Data {
+				rows = append(rows, []string{
+					fmt.Sprintf("%.0f", toFloat(v["vmid"])),
+					toString(v["name"]),
+					toString(v["status"]),
+					fmt.Sprintf("%.0f", toFloat(v["maxmem"])/1024/1024),
+				})
+			}
+
+			output.Table(headers, rows)
+
+			return nil
+		},
+	}
+
+	cmd.Flags().StringVar(&node, "node", "", "Proxmox node name")
+
+	return cmd
+}
